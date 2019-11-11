2f4bcdb99e5597e96c185104fa4cae0b0022c5d8
Hennes Märtins <hmaertins@avides.com>
2019-11-11 17:33:11 +0100

    Update Docker image to 4.0.14-alpine

--- src/main/java/com/avides/springboot/testcontainer/redis/RedisProperties.java
+++ src/main/java/com/avides/springboot/testcontainer/redis/RedisProperties.java
@@ -20,6 +20,6 @@ public class RedisProperties extends AbstractEmbeddedContainerProperties
 
     public RedisProperties()
     {
-        setDockerImage("redis:4.0.12-alpine");
+        setDockerImage("redis:4.0.14-alpine");
     }
 }
